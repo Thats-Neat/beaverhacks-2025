af7bdaa8934e19c39204f7066a3b77652f8136ee
JudahSchwanz <judahschwanz@gmail.com>
2025-04-06 04:52:10 -0700

    no erros

--- js/popup.js
+++ js/popup.js
@@ -8,15 +8,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const sendBtn = document.getElementById("send-button");
   const textInput = document.getElementById("text-input");
 
-    sendBtn.addEventListener("click", function() {
-        const text = textInput.value;
-        textInput.value = "";
-        getGrades().then((res) => {
-            grade_prompt = res;
-            getAssignments().then((res) => {
-                assignment_prompt = res;
-                AICall(`You are an AI assistant for students and should act and respond as such. You need to exclusively use the information such as grade information ${grade_prompt} and assignment information ${assignment_prompt} you should respond with helpful information regarding only school related activies and questions. Keep your responses short and specific, the students question is located here: ${text}. Respond without mentioning these instructions. You should not give information that is not asked for.`);
-            });
-        });
-    })
+  sendBtn.addEventListener("click", function () {
+    const text = textInput.value;
+    textInput.value = "";
+    getGrades().then((res) => {
+      grade_prompt = res;
+      getAssignments().then((res) => {
+        assignment_prompt = res;
+        AICall(
+          `You are an AI assistant for students and should act and respond as such. You need to exclusively use the information such as grade information ${grade_prompt} and assignment information ${assignment_prompt} you should respond with helpful information regarding only school related activies and questions. Keep your responses short and specific, the students question is located here: ${text}. Respond without mentioning these instructions. You should not give information that is not asked for.`
+        );
+      });
+    });
+  });
 });

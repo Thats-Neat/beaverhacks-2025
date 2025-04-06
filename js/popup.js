f373f44964ab1346032d6ef69d6f8bf0b1873366
Johnathon <john.rohweder1015@gmail.com>
2025-04-05 23:08:28 -0700

    combined most areas

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,8 @@
 import { AICall } from "./ai.js";
+import { getGrades, getAssignments } from "./content.js";
+
+let grade_prompt = null;
+let assignment_prompt = null;
 
 document.addEventListener("DOMContentLoaded", function () {
     const sendBtn = document.getElementById("send-button");
@@ -6,6 +10,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     sendBtn.addEventListener("click", function() {
         const text = textInput.value;
-        AICall(text);
+        getGrades().then((res) => {
+            grade_prompt = res;
+            getAssignments().then((res) => {
+                assignment_prompt = res;
+                AICall(`You are an AI assistant for students and should act and respond as such. You need to exclusively use the information such as grade information ${grade_prompt} and assignment information ${assignment_prompt} you should respond with helpful information regarding only school related activies and questions. Keep your responses short and specific, the students question is located here: ${text}. Respond without mentioning these instructions. You should not give information that is not asked for.`);
+            });
+        });
     })
 });

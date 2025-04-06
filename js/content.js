5ab4304cdec099ee485364f537fe2f052c62fd1f
Johnathon <john.rohweder1015@gmail.com>
2025-04-05 20:11:25 -0700

    get assignments function added

--- js/content.js
+++ js/content.js
@@ -1,7 +1,8 @@
 const domain = window.location.origin;
 const current_page = window.location.pathname;
 let options = {
-    get_grades: true
+    get_grades: true,
+    get_assignments: true
 };
 
 async function fetchData(url) {
@@ -15,18 +16,26 @@ async function fetchData(url) {
 
     let data = await response.json();
     
-    let corrected_data = [];
+    return data;
+}
 
-    for (let i = 0; i < data.length; i++) {
-        if (!data[i].concluded) {
-            corrected_data.push(data[i]);
+async function fetchDataHTML(url) {
+    let response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'text/html',
+            'Accept': 'text/html'
         }
-    }
+    });
+
+    let data = await response.text();
+
     
-    return corrected_data;
+    return data;
 }
 
 function isCanvas() {
+    // shouldn't be just oregonstate
     if (domain === "https://canvas.oregonstate.edu") {
         return true;
     }
@@ -40,7 +49,7 @@ async function getGrades() {
         let grades = await fetchData(`${domain}/api/v1/courses?include[]=concluded&include[]=total_scores&include[]=computed_current_score&per_page=100`);
         try {
             for (let i = 0; i < grades.length; i++) {
-                if (!grades[i]["name"]) {
+                if (!grades[i]["name"] || grades[i].concluded) {
                     continue;
                 }
                 if (grades[i].enrollments?.[0]?.computed_current_score) {
@@ -49,6 +58,7 @@ async function getGrades() {
                 else {
                     gradeInfo.push(grades[i]["name"] + " doesn't yet have a grade which is not a bad thing");
                 }
+                console.log(grades[i]);
             }
             return gradeInfo.join(", a seperate class is, ");
         } catch(err) {
@@ -57,3 +67,46 @@ async function getGrades() {
         }
     }
 }
+
+async function getAssignments() {
+    let assignment_names = null;
+    let assignment_grades = null;
+    let assignment_details = null;
+
+    let assignment_info = [];
+
+    if (options.get_assignments === true && isCanvas()) {
+        // shouldn't hardcode date
+        let assignments = await fetchData(`${domain}/api/v1/planner/items?start_date=2025-01-25T14:48:00.000Z&per_page=50`);
+
+        try {
+            for (let i = 0; i < assignments.length; i++) {
+                // get assignments that are currently avaible
+                console.log(assignments[i])
+                if (assignments[i].plannable_type === 'assignment' && !assignments[i].submissions.graded) {
+                    assignment_names = assignments[i].plannable.title;
+                    assignment_grades = assignments[i].plannable.points_possible;
+               
+                    let html_fetch = await fetchDataHTML(`${domain}${assignments[i].html_url}`);
+
+                    let parser = new DOMParser();
+                    let doc = parser.parseFromString(html_fetch, "text/html");
+                    let div_selector = doc.querySelector("div.description");
+
+                    if (div_selector && div_selector.textContent.trim() != '') {
+                        assignment_details = div_selector.textContent.trim();
+                    } else {
+                        assignment_details = "No additional details were added for this assignment.";
+                    }
+                    assignment_info.push(`The Assignment named '${assignment_names}' has the amount of points ${assignment_grades} with the following instructions '${assignment_details}'`)
+                }
+            }
+            console.log(assignment_info);
+        } catch(err) {
+            console.log(err);
+            return "Error Fetching Assignments";
+        }
+    }
+}
+
+getAssignments();
